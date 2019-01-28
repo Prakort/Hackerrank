926f340650c8b9115fb64e701ece1ef4cd068bb3
Prakort Lean <46115782+prakort@users.noreply.github.com>
2019-01-28 13:44:14 -0500

    Create Minimun2Swaps.java

---
+++ Minimun2Swaps.java
@@ -0,0 +1,21 @@
+// Complete the minimumSwaps function below.
+
+      static int minimumSwaps(int[] arr) {
+        int first = 0, last = arr.length - 1;
+        int swaps = 0;
+        while (first < last) {
+            while (arr[first] == first + 1 && first < last)
+                first++;
+            while (arr[last] == last + 1 && first < last)
+                last--;
+            if (first < last) {
+                int temp = arr[arr[first] - 1];
+                arr[arr[first] - 1] = arr[first];
+                arr[first] = temp;
+                swaps++;
+            }
+        }
+        return swaps;
+    
+
+}
