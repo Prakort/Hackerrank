6eb6bcd96942ae5d8d1d737b31bf94ccdfe3e98e
Prakort Lean <46115782+prakort@users.noreply.github.com>
2019-01-28 13:32:33 -0500

    Create Solution.java

---
+++ Sock Merchant/Solution.java
@@ -0,0 +1,34 @@
+
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+
+        Arrays.sort(ar);
+        Set<Integer> unique= new LinkedHashSet<Integer>();
+        for (int x : ar)
+            unique.add(x);
+        
+        int count=0;
+        int i =0;
+        int []countInt = new int[n];
+        for (int x: unique)
+        {
+            for (int c: ar)
+            {
+                if ( x == c)
+                    countInt[i]++;        
+                    
+            }
+            i++;
+        }
+        for ( int d : countInt)
+        {
+            if(d!=0)
+                count = count + d/2;
+        }
+        return count;
+        
+    }
+
+  
